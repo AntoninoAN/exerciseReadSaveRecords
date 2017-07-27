a4d2d05a454c54e1479d20f5e51cbb62d094c120
AntoninoAN <andonino@gmail.com>
2017-07-27 16:08:18 -0400

    Udpate and Delete

--- app/src/main/java/com/example/admin/w2d3_ex03/MainActivity.java
+++ app/src/main/java/com/example/admin/w2d3_ex03/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = MainActivity.class.getSimpleName()+"_TAG";
     private DBHelper helper;
     private SQLiteDatabase database;
-    public EditText et_tittlevalue,et_subtitlevalue;
-    public Button btnSave,btnRead;
+    public EditText et_tittlevalue,et_subtitlevalue,et_updatevalue;
+    public Button btnSave,btnRead,btnUpdate,btnDelete;
     public TextView tv_presentvalue;
     StringBuilder stringBuilder= new StringBuilder();
 
@@ -33,10 +33,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         et_tittlevalue=(EditText)findViewById(R.id.et_Title);
         et_subtitlevalue=(EditText)findViewById(R.id.et_Subtitle);
         tv_presentvalue=(TextView)findViewById(R.id.tv_PresentValues);
+        et_updatevalue=(EditText)findViewById(R.id.et_updateValue);
         btnSave=(Button)findViewById(R.id.btn_SaveValue);
         btnSave.setOnClickListener(this);
         btnRead=(Button)findViewById(R.id.btn_ReadValue);
         btnRead.setOnClickListener(this);
+        btnUpdate=(Button)findViewById(R.id.btn_UpdateValue);
+        btnUpdate.setOnClickListener(this);
+        btnDelete=(Button)findViewById(R.id.btn_DeleteValue);
+        btnDelete.setOnClickListener(this);
     }
 
     @Override
@@ -47,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_ReadValue:
                 readRecord();
+                break;
+            case R.id.btn_UpdateValue:
+                break;
+            case R.id.btn_DeleteValue:
                 break;
         }
 
@@ -107,6 +116,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             tv_presentvalue.setText("");
             tv_presentvalue.setText(stringBuilder);
             Log.d(TAG, "readRecord: id: " + entryId+" title: "+entryTitle+" subtitle: "+entrySubtitle);
+        }
+    }
+    private void deleteRecord(){
+        String selection= FeedEntry.COLUMN_NAME_TITLE+" LIKE ?";
+        if(checkEmptyTitleValue()){
+            Toast.makeText(MainActivity.this,"NOT ALLOW DELETE EMPTY VALUES",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String[] selectionArgs = {et_tittlevalue.getText().toString()};
+            int deleted = database.delete(
+                    FeedEntry.TABLE_NAME,
+                    selection,
+                    selectionArgs
+            );
+            if (deleted > 0) {
+                Log.d(TAG, "deletedRecord: record deleted");
+                Toast.makeText(MainActivity.this, "RECORD DELETED " + et_tittlevalue.getText().toString(), Toast.LENGTH_SHORT).show();
+                et_tittlevalue.setText("");
+            } else {
+                Log.d(TAG, "deletedRecord: record not deleted");
+                Toast.makeText(MainActivity.this, "RECORD NOT DELETED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public Boolean checkEmptyTitleValue(){
+        return et_tittlevalue.getText().toString().isEmpty();
+    }
+    private void updateRecord(){
+        ContentValues values= new ContentValues();
+        values.put(FeedEntry.COLUMN_NAME_TITLE, et_updatevalue.getText().toString().isEmpty()?
+                "EMPTY VALUE":et_updatevalue.getText().toString());
+
+        String selection= FeedEntry.COLUMN_NAME_TITLE+" LIKE ?";
+        if(checkEmptyTitleValue()){
+            Toast.makeText(MainActivity.this,"NOT ALLOW UPDATE EMPTY VALUES",Toast.LENGTH_SHORT).show();
+            et_updatevalue.setText("");
+        }
+        else {
+            String[] selectionArgs = {et_tittlevalue.getText().toString()};
+            int count = database.update(FeedEntry.TABLE_NAME, values, selection, selectionArgs);
+            if (count > 0) {
+                tv_presentvalue.setText("");
+                tv_presentvalue.setText("Updated "+et_tittlevalue+" for: "+et_updatevalue);
+                Log.d(TAG, "updatedRecord: Records Updated");
+                et_tittlevalue.setText("");
+                et_updatevalue.setText("");
+            } else
+                Log.d(TAG, "updatedRecord: No Records Updated");
         }
     }
 }
